54759b3176b9d9990b2a12de147e53dc6302e615
Meherun Akter <116157144+meherun05@users.noreply.github.com>
2023-07-10 22:07:37 +0600

    Add files via upload

---
+++ java practice/BasicJava.java
@@ -0,0 +1,31 @@
+class BasicJava {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World");
+
+            byte b = 20;
+             System.out.println("b="+ b);
+             
+             short s =10;
+             System.out.println("s="+s);
+             
+             int i =15;
+             System.out.println("i="+i);
+             
+             long l= 23;
+             System.out.println("l="+l);
+             
+             float f = 25;
+             System.out.println("f="+ f);
+             
+             double d = 20.25;
+             System.out.println("d="+ d);
+             
+             char ch = 'S';
+             System.out.println("ch="+ch);
+             
+             char ch1 ='&';
+             System.out.println("ch1="+ch1);
+     
+    }
+}
